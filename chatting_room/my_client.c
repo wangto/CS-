6cd1c5bc3b34aad8b93ac05959a10c6f9fa0e634
wangto <wangtong19951128@qq.com>
2015-08-08 15:50:34 -0400

    CS聊天程序

---
+++ chatting_room/my_client.c
@@ -0,0 +1,33 @@
+/*************************************************************************
+	> File Name: my_client.c
+	> Author: WT
+	> Mail: wangtong19951128@qq.com 
+	> Created Time: Sat 08 Aug 2015 03:41:29 PM EDT
+ ************************************************************************/
+
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+
+int sys_init()
+{
+    int sock_fd 
+    
+        
+        
+        
+        
+        
+        
+}
+
+int main(void)
+{
+
+
+
+
+
+
+
+}
